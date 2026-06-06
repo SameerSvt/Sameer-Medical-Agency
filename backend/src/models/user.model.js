3493b0f4a098c1021800a0430063673549481d67
Sameer Srivastav <sameer.svt852005@gmail.com>
2026-06-06 14:55:46 +0530

    changes made in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema({
     fullName: { type: String, required: true, trim: true },
@@ -36,6 +37,33 @@ userSchema.pre("save", async function(next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign (
+        {
+            _id: this._id,
+            fullName: this.fullName,
+            email: this.email,
+            phone: this.phone
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign (
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 
